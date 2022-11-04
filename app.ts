105d1e0b8728bfdde417ffeb8836cec301fda108
dennishoooo <105833202+dennishoooo@users.noreply.github.com>
2022-11-04 16:10:44 +0800

    initial commit

---
+++ app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const port = 8080;
+
+console.log(__dirname);
+
+app.get("/", (req, res) => {
+  res.end("hi");
+});
+
+app.use((req, res) => {
+  let filePath = path.join(__dirname, "public", "404.html");
+  console.log(filePath);
+  res.sendFile(path.join(filePath));
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
+
+// /Users/dennisho/code/wsp-demo
+
+// /Users/dennisho/code/wsp-demo/public/404.html
